de94179701ce7a631a91fed458f0768d4fe65f25
Vixen <30753940+Vixenaustralia@users.noreply.github.com>
2020-09-26 16:03:30 +0930

    Add files via upload

---
+++ tokovoip_script/s_config.lua
@@ -0,0 +1,13 @@
+TokoVoipConfig = {
+	channels = {
+		{name = "SAPF/SAESA Shared Ch 1", subscribers = {}},
+		{name = "SAESA Shared Ch 1", subscribers = {}},
+		{name = "SAPF Shared Ch 1", subscribers = {}},
+		{name = "SAPF Los Santos City Ch 1", subscribers = {}},
+		{name = "SAPF Northern Lakes Ch 1", subscribers = {}},
+		{name = "SAESA Metro Ch 1", subscribers = {}},
+		{name = "SAESA Northern Ch 1", subscribers = {}},
+		{name = "SAAS Shared Ch 1", subscribers = {}},
+		{name = "SAFS Shared Ch 1", subscribers = {}}
+	}
+};
